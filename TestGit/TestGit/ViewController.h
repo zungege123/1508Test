dd4f84ad15d18613a98dffee99a758438f1ca4ac
zunzun <1243760832@qq.com>
2015-12-01 10:05:54 +0800

    testtest

---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by tarena on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGit/TestGitTests/TestGitTests.m
@@ -0,0 +1,39 @@
+//
+//  TestGitTests.m
+//  TestGitTests
+//
+//  Created by tarena on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestGitTests : XCTestCase
+
+@end
+
+@implementation TestGitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
